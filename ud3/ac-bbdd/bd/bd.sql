ba03797789d5546aa7ac9edd6384e5d4a559f52e
deo <jorge.r.9763@gmail.com>
2025-11-19 13:56:34 +0100

    inicio practica

---
+++ ud3/ac-bbdd/bd/bd.sql
@@ -0,0 +1,3 @@
+#Aquí tu archivo sql que contine la información de tu base de datos
+#En este directorio bambién puedes incluir un script de creación de tu bbdd y/o tablas si detectas 
+# que tienes acceso al servidor de bbdd pero tu estructura de datos no está montada en el servidor.
---
+++ ud3/ac-bbdd/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cls/bbdd',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '04de7f34f59cf9fc2ff3d2a9046a919f3210ae8d',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cls/bbdd' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '04de7f34f59cf9fc2ff3d2a9046a919f3210ae8d',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
